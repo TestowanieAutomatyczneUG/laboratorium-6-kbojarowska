04279a0c8f8adc6846ea4d020957a26fbfa39971
kbojarowska <kbojarowska@wp.pl>
2021-11-17 21:23:13 +0100

    Dodano możliwość otrzymania wybranego fragmentu piosenki

--- Zadanie3.py
+++ Zadanie3.py
@@ -21,6 +21,10 @@ On the twelfth day of Christmas my true love gave to me: twelve Drummers Drummin
         elif howMany == "all":
             return song
 
+        elif type(howMany) is tuple:
+            songList = song.split("\n")
+            return "\n".join(songList[howMany[0]-1:howMany[1]])
+
 
 lines = Song().lines
 
@@ -45,16 +49,14 @@ On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eigh
 On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
 On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
 On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.""")
-
-    @unittest.skip("not done yet")  
+ 
     def test_lines_from_one_to_three(self):
         self.assertEqual(lines((1,3)),"""On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
 On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.
 On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.""")
 
-    @unittest.skip("not done yet")
     def test_lines_from_one_to_nine(self):
-        self.assertEqual(lines(9), """On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
+        self.assertEqual(lines((1,9)), """On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
 On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.
 On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
 On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
@@ -64,12 +66,10 @@ On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming,
 On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
 On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.""")
 
-    @unittest.skip("not done yet")
     def test_lines_from_six_to_seven(self):
         self.assertEqual(lines((6,7)), """On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
 On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.""")
 
-    @unittest.skip("not done yet")
     def test_lines_from_five_to_fife(self):
         self.assertEqual(lines((5,5)), """On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.""")
    
