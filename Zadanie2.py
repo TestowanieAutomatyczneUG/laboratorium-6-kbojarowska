77138a3b3271f8ae3c6ca134bb6365b3d1ae5475
kbojarowska <kbojarowska@wp.pl>
2021-11-17 12:08:28 +0100

    Zmieniono pomysł na zadanie, użyto słowniki

--- Zadanie2.py
+++ Zadanie2.py
@@ -2,12 +2,11 @@ import unittest
 
 class RomanNumerals:
     def roman(self, num):
-        if num<4:
-            return "I"*num
-        elif num==4:
-            return "IV"
-        elif num<9:
-            return "V"+("I"*(num-5))
+        romanDict={1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X', 20: 'XX',
+                30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC', 100: 'C', 200: 'CC', 300: 'CCC',
+                400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'DCCC', 900: 'CM', 1000: 'M', 2000: 'MM', 3000: 'MMM'}
+        if num in romanDict:
+            return romanDict[num]
 
 roman = RomanNumerals().roman
 
@@ -31,7 +30,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_6_being_5_1_is_vi(self):
         self.assertEqual(roman(6), "VI")
 
-    @unittest.skip("not done yet")
     def test_9_being_10_1_is_ix(self):
         self.assertEqual(roman(9), "IX")
 
