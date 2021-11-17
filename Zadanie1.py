71b7b9f6103e28a3571e9009be1400f094ba30b5
kbojarowska <kbojarowska@wp.pl>
2021-11-17 10:52:23 +0100

    Zaczęto zadanie 1

---
+++ Zadanie1.py
@@ -0,0 +1,52 @@
+import unittest
+
+class HammingTest(unittest.TestCase):
+
+    @unittest.skip("not done yet")
+    def test_empty_strands(self):
+        self.assertEqual(hamming.distance("", ""), 0)
+
+    @unittest.skip("not done yet")
+    def test_single_letter_identical_strands(self):
+        self.assertEqual(hamming.distance("A", "A"), 0)
+    
+    @unittest.skip("not done yet")
+    def test_single_letter_different_strands(self):
+        self.assertEqual(hamming.distance("G", "T"), 1)
+
+    @unittest.skip("not done yet")
+    def test_long_identical_strands(self):
+        self.assertEqual(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
+
+    @unittest.skip("not done yet")
+    def test_long_different_strands(self):
+        self.assertEqual(hamming.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
+
+    @unittest.skip("not done yet")
+    def test_disallow_first_strand_longer(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("AATG", "AAA")
+
+    @unittest.skip("not done yet")
+    def test_disallow_second_strand_longer(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("ATA", "AGTG")
+
+    @unittest.skip("not done yet")
+    def test_disallow_left_empty_strand(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("", "G")
+
+    @unittest.skip("not done yet")
+    def test_disallow_right_empty_strand(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("G", "")
+
+    # Utility functions
+    def setUp(self):
+        try:
+            self.assertRaisesRegex
+        except AttributeError:
+            self.assertRaisesRegex = self.assertRaisesRegexp
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
